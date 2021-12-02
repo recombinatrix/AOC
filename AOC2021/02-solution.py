2f59b75968ce11d0d68db5a9a0c4bf4d7d9bfe7c
recombinatrix <ada.s.quinn+github@gmail.com>
2021-12-02 17:31:24 +1100

    day 02

---
+++ AOC2021/02-solution.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#generate three scalars from the input file
+
+up = sum(np.loadtxt("input/02", comments=["do", "fo"],dtype=str)[:,1].astype(np.int8)) 
+down = sum(np.loadtxt("input/02", comments=["up", "fo"],dtype=str)[:,1].astype(np.int8))
+forward = sum(np.loadtxt("input/02", comments=["up", "do"],dtype=str)[:,1].astype(np.int8)) #comment needs to be "do" not "d" because there is a d in forward
+print("part one: " + str(across * (down - up)))
+
+# oh well, this won't work at all for part 2
+
+nav = np.loadtxt("input/02",dtype=str)
+nav[:,1].astype(np.int8)
+
+pos = [0, 0]
+aim = 0
+
+for (a, b) in nav:
+    if a == "up":
+        aim -= int(b)
+    if a == "down":
+        aim += int(b)
+    if a == "forward":
+        pos[0] += int(b)
+        pos[1] += (int(b) * aim)
+
+print("part two: " + str(pos[0] * pos[1]))
+
+# going to see if I can come up with a more elegant solution to this
