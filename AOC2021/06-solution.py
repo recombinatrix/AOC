6765e8b3bde21154f5ad9a827fd5e330fb4145ee
recombinatrix <ada.s.quinn+github@gmail.com>
2021-12-06 20:26:14 +1100

    day 06

--- AOC2021/06-solution.py
+++ AOC2021/06-solution.py
@@ -1,9 +1,13 @@
+
 import numpy as np
 data = np.loadtxt("input/06", delimiter=",", dtype=int)
 fish = np.zeros((9,1), dtype=int)
 
 def fishbreeder(arr, days):
+    popgraph = np.zeros((days,2), dtype=int)
     for day in range(days):
+        popgraph[day,0] = day
+        popgraph[day,1] = np.sum(arr[:,0])
         new = np.zeros((9,1), dtype=int)
         for i in np.ndenumerate(arr):
             if i[0][0] == 0:
@@ -12,12 +16,18 @@ def fishbreeder(arr, days):
             else:
                 new[i[0][0]-1,0] += i[1]
         arr = new
-    return(np.sum(arr[:,0]))
+    # returns a tuple. [0] is n(fish) on the final day, [1] is a 2d array of days versus n(fish), for graphing
+    return(np.sum(arr[:,0]), popgraph)
 
 for i in range(9): 
     count = np.count_nonzero(data == i)
-    total -= count 
     fish[i,0] = count
 
-print(fishbreeder(fish,80))
-print(fishbreeder(fish,256))
+day80 = fishbreeder(fish,80)
+print(day80[0])
+day256 = fishbreeder(fish,256)
+print(day256[0])
+
+#alternate method based on exponents
+# print("part2:", int(1.11622269641992 ** 256))
+
