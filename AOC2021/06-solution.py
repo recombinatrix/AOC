9a467b589d3a0eca340eeaaf46d693a768247c40
recombinatrix <ada.s.quinn+github@gmail.com>
2021-12-07 14:37:31 +1100

    day 06 cleaned readme

--- AOC2021/06-solution.py
+++ AOC2021/06-solution.py
@@ -5,6 +5,14 @@ fish = np.zeros((9,1), dtype=int)
 
 
 def fishbreeder(arr, days):
+    """Calculate the number of lapfish from a starting population after a certain number of days. 
+    Takes (arr, days). 
+    arr is a (9,1) array where arr[n] is the number of fish at with n days left before breeding 
+    days is the number of days to breed fish 
+    Returns a tuple with two elements
+    fishfinder[0] is the number of fish after the final day
+    fishfinder[1] containing the number of fish at the start of each day 
+    because the value of fishfinder[1] is the start of the day before breeding, the size of popgraph is days + 1"""
     popgraph = np.zeros((days+1,1), dtype=int) #to store number of fish at end of each day
     for day in range(days):
         if (day) == 0:
@@ -18,9 +26,6 @@ def fishbreeder(arr, days):
                 new[i[0][0]-1,0] += i[1]
         arr = new
         popgraph[day+1,0] = np.sum(arr[:,0])
-    # tuple[0] is n(fish) on the final day
-    # tuple[1] contains a 1d array of n(fish) at the start of day n
-    # because the value is the start of the day before breeding, the size of popgraph is days + 1
     return(np.sum(arr[:,0]), popgraph)
 
 
