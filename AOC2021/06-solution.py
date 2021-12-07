d951bb57c0ddf1b2ccf0d405ae74097a38916893
recombinatrix <ada.s.quinn+github@gmail.com>
2021-12-07 11:51:27 +1100

    Day 06 - improved to return n(fish) at start of each day

--- AOC2021/06-solution.py
+++ AOC2021/06-solution.py
@@ -3,11 +3,12 @@ import numpy as np
 data = np.loadtxt("input/06", delimiter=",", dtype=int)
 fish = np.zeros((9,1), dtype=int)
 
+
 def fishbreeder(arr, days):
-    popgraph = np.zeros((days,2), dtype=int)
+    popgraph = np.zeros((days+1,1), dtype=int) #to store number of fish at end of each day
     for day in range(days):
-        popgraph[day,0] = day
-        popgraph[day,1] = np.sum(arr[:,0])
+        if (day) == 0:
+            popgraph[day,0] = np.sum(arr[:,0])
         new = np.zeros((9,1), dtype=int)
         for i in np.ndenumerate(arr):
             if i[0][0] == 0:
@@ -16,18 +17,19 @@ def fishbreeder(arr, days):
             else:
                 new[i[0][0]-1,0] += i[1]
         arr = new
-    # returns a tuple. [0] is n(fish) on the final day, [1] is a 2d array of days versus n(fish), for graphing
+        popgraph[day+1,0] = np.sum(arr[:,0])
+    # tuple[0] is n(fish) on the final day
+    # tuple[1] contains a 1d array of n(fish) at the start of day n
+    # because the value is the start of the day before breeding, the size of popgraph is days + 1
     return(np.sum(arr[:,0]), popgraph)
+
 
 for i in range(9): 
     count = np.count_nonzero(data == i)
     fish[i,0] = count
 
-day80 = fishbreeder(fish,80)
-print(day80[0])
-day256 = fishbreeder(fish,256)
-print(day256[0])
+print(fishbreeder(fish,80)[0])
+print(fishbreeder(fish,256)[0])
 
 #alternate method based on exponents
 # print("part2:", int(1.11622269641992 ** 256))
-
